5edd2900cbaf28d7886a6c0582e7d04936711f05
康康 <triplehk830@larvata.tw>
2024-07-19 20:22:28 +0800

    fix: api

--- src/views/form/list.vue
+++ src/views/form/list.vue
@@ -32,7 +32,7 @@
           {{ t('form.action.view') }}
         </el-button>
         <el-button
-          v-auth="['managerUpdate']"
+          v-auth="['formUpdate']"
           type="primary"
           link
           :icon="EditPen"
@@ -43,12 +43,11 @@
         </el-button>
 
         <el-button
-          v-auth="['managerDelete']"
+          v-auth="['formDelete']"
           type="primary"
           link
           :icon="Delete"
           @click="deleteAccount(scope.row)"
-          :disabled="scope.row.id === userStore.userInfo.id"
         >
           {{ t('form.action.delete') }}
         </el-button>
@@ -65,7 +64,6 @@ import { ElMessage, ElMessageBox } from 'element-plus'
 import { View, EditPen, CirclePlus, Delete, WarningFilled } from '@element-plus/icons-vue'
 import { useI18n } from 'vue-i18n'
 import ProTable from '@/components/ProTable/index.vue'
-import { adminStatus } from '@/utils/dict'
 import { toDate } from '@/utils/day'
 import UserDrawer from '@/views/form/component/from.vue'
 import { useUserStore } from '@/stores/modules/user'
@@ -73,7 +71,6 @@ import { useAuthStore } from '@/stores/modules/auth'
 import * as zod from 'zod'
 import { ACCOUNT_RULE, USERNAME_REGEXP } from '@/config/validate'
 import { CreateRequest, ListRequest, UpdateRequest, DeleteRequest } from '@/api/form/form'
-import { RoleRequest } from '@/api/selection/selection'
 
 const userStore = useUserStore()
 const authStore = useAuthStore()
@@ -93,20 +90,7 @@ const params = ref({
 const { t } = useI18n()
 
 const getTableList = (params) => {
-  if (
-    authStore.authButtonListGet.includes('managerDirectList') &&
-    authStore.authButtonListGet.includes('managerList')
-  ) {
-    return ListRequest(params)
-  }
-
-  if (authStore.authButtonListGet.includes('managerDirectList')) {
-    return ListDirectRequest(params)
-  }
-
-  if (authStore.authButtonListGet.includes('managerList')) {
-    return ListRequest(params)
-  }
+  ListRequest(params)
 }
 
 // 如果表格需要初始化请求参数，直接定义传给 ProTable (之后每次请求都会自动带上该参数，此参数更改之后也会一直带上，改变此参数会自动刷新表格数据)
@@ -127,8 +111,8 @@ const sortTable = () => {
   ElMessage.success('修改列表排序成功')
 }
 
-function deleteAccount({ id, username }) {
-  ElMessageBox.confirm(t('common.confirmDelete', { username }), '', {
+function deleteAccount({ id, name }) {
+  ElMessageBox.confirm(t('common.confirmDelete', { name }), '', {
     confirmButtonText: 'OK',
     type: 'warning',
     icon: markRaw(WarningFilled),
@@ -138,7 +122,7 @@ function deleteAccount({ id, username }) {
       DeleteRequest({ id })
         .then(() => {
           const message = `${t(`form.action.delete`, {
-            target: username
+            target: name
           })} ${t('common.success')}`
           ElMessage.success({ message })
           proTable.value?.getTableList()
@@ -158,65 +142,64 @@ const columns = reactive([
     prop: 'username',
     label: t('form.fields.username'),
     search: { el: 'input' },
-    render: (scope) => <span v-copy={scope.row.username}>{scope.row.username}</span>,
-    width: 120
+    render: (scope) => <span v-copy={scope.row.name}>{scope.row.name}</span>
   },
-  { prop: 'nickname', label: t('form.fields.nickname') },
-  {
-    prop: 'mobile',
-    label: t('form.fields.mobile'),
-    render: (scope) => <span v-copy={scope.row.mobile}>{scope.row.mobile}</span>
-  },
-  {
-    prop: 'whatapps',
-    label: t('form.fields.whatapps'),
-    render: (scope) => <span v-copy={scope.row.whatapps}>{scope.row.whatapps}</span>
-  },
-  {
-    prop: 'email',
-    label: t('form.fields.email'),
-    render: (scope) => <span v-copy={scope.row.email}>{scope.row.email}</span>
-  },
-  {
-    prop: 'status',
-    label: t('form.fields.status'),
-    enum: adminStatus(),
-    search: { el: 'select', props: { filterable: true } },
-    fieldNames: { label: 'label', value: 'value' },
-    render: (scope) => {
-      return (
-        <el-tag type={scope.row.status === 1 ? 'success' : 'danger'}>
-          {scope.row.status ? t(`enum.adminStatus.${scope.row.status}`) : '---'}
-        </el-tag>
-      )
-    },
-    width: 100
-  },
-  {
-    prop: 'roleIds',
-    label: t('form.fields.roleIds'),
-    enum: RoleRequest,
-    search: { el: 'select-v2', props: { filterable: true, multiple: true } },
-    fieldNames: { label: 'label', value: 'value' },
-    render: (scope) => {
-      const row = []
-      for (const i of scope.row.roleIds) {
-        const idx = proTable.value?.enumMap.get('roleIds').findIndex((o) => i === o.value)
-        if (idx !== -1) {
-          row.push(<el-tag type="info">{proTable.value?.enumMap.get('roleIds')[idx].label}</el-tag>)
-          row.push(<br />)
-        }
-      }
-      return row
-    }
-  },
-  { prop: 'description', label: t('form.fields.description') },
+  // { prop: 'nickname', label: t('form.fields.nickname') },
+  // {
+  //   prop: 'mobile',
+  //   label: t('form.fields.mobile'),
+  //   render: (scope) => <span v-copy={scope.row.mobile}>{scope.row.mobile}</span>
+  // },
+  // {
+  //   prop: 'whatapps',
+  //   label: t('form.fields.whatapps'),
+  //   render: (scope) => <span v-copy={scope.row.whatapps}>{scope.row.whatapps}</span>
+  // },
+  // {
+  //   prop: 'email',
+  //   label: t('form.fields.email'),
+  //   render: (scope) => <span v-copy={scope.row.email}>{scope.row.email}</span>
+  // },
+  // {
+  //   prop: 'status',
+  //   label: t('form.fields.status'),
+  //   enum: adminStatus(),
+  //   search: { el: 'select', props: { filterable: true } },
+  //   fieldNames: { label: 'label', value: 'value' },
+  //   render: (scope) => {
+  //     return (
+  //       <el-tag type={scope.row.status === 1 ? 'success' : 'danger'}>
+  //         {scope.row.status ? t(`enum.adminStatus.${scope.row.status}`) : '---'}
+  //       </el-tag>
+  //     )
+  //   },
+  //   width: 100
+  // },
+  // {
+  //   prop: 'roleIds',
+  //   label: t('form.fields.roleIds'),
+  //   enum: RoleRequest,
+  //   search: { el: 'select-v2', props: { filterable: true, multiple: true } },
+  //   fieldNames: { label: 'label', value: 'value' },
+  //   render: (scope) => {
+  //     const row = []
+  //     for (const i of scope.row.roleIds) {
+  //       const idx = proTable.value?.enumMap.get('roleIds').findIndex((o) => i === o.value)
+  //       if (idx !== -1) {
+  //         row.push(<el-tag type="info">{proTable.value?.enumMap.get('roleIds')[idx].label}</el-tag>)
+  //         row.push(<br />)
+  //       }
+  //     }
+  //     return row
+  //   }
+  // },
+  // { prop: 'description', label: t('form.fields.description') },
   {
     prop: 'createdAt',
     label: t('form.fields.createdAt'),
     render: (scope) => toDate(scope.row.createdAt)
   },
-  { prop: 'operation', label: t('form.fields.operation'), fixed: 'right', width: 100 }
+  { prop: 'operation', label: t('form.fields.operation'), fixed: 'right' }
 ])
 
 // 打开 drawer(新增、查看、编辑)
