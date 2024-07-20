c07904cbc734594c2a16b587c09189c9a0bfd4bf
康康 <triplehk830@larvata.tw>
2024-07-20 18:27:58 +0800

    fix: api

--- src/api/form/form.js
+++ src/api/form/form.js
@@ -4,3 +4,8 @@ export const ListRequest = (params) => http.post('form/list', params)
 export const CreateRequest = (params) => http.post('form/create', params)
 export const UpdateRequest = (params) => http.post('form/update', params)
 export const DeleteRequest = (params) => http.post('form/delete', params)
+
+export const IntermediateListRequest = (params) => http.post('form/intermediate/list', params)
+export const IntermediateCreateRequest = (params) => http.post('form/intermediate/create', params)
+export const IntermediateUpdateRequest = (params) => http.post('form/intermediate/update', params)
+export const IntermediateDeleteRequest = (params) => http.post('form/intermediate/delete', params)
